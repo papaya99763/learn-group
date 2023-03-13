b11426f53f3557f150727fe106e884d5b41b3da0
waylon.yuen <waylon.yuen@gosmio.biz>
2023-03-13 16:56:15 +0800

    Add : 添加 list 宣告

--- src/list.c
+++ src/list.c
@@ -12,3 +12,43 @@ void Show_Invert(int arr[], int size) {
         printf("%d ", arr[i]);
     }
 }
+
+int At(int arr[], int size, int idx) {
+    // todo: 
+    return -1;
+}
+
+bool Find(int arr[], int size, int value, int *result) {
+    // todo: 
+    return false;
+}
+
+bool Remove(int arr[], int size, int idx) {
+    // todo: 
+    return false;
+}
+
+bool Insert(int arr[], int size, int idx, int value) {
+    // todo: 
+    return false;
+}
+
+bool Append(int arr[], int size, int value) {
+    // todo: 
+    return false;
+}
+
+bool Invert(int arr[], int size) {
+    // todo: 
+    return false;
+}
+
+bool Join(int ori[], int ori_size, int join[], int join_size) {
+    // todo: 
+    return false;
+}
+
+bool Sort(int arr[], int size, int type) {
+    // todo: 
+    return false;
+}
--- src/list.h
+++ src/list.h
@@ -3,12 +3,30 @@
 #ifndef LIST_H
 #define LIST_H
 
+#define bool int
+#define true 1
+#define false 0
+
 void Show_Sequence(int arr[], int size);
 
 void Show_Invert(int arr[], int size);
 
-//void Find_Array(int arr[], int size, int idx);
+int At(int arr[], int size, int idx);
 
-//void Modify_array(int arr[], int size, int targetIdx, int newValue);
+bool Find(int arr[], int size, int value, int *result);
+
+bool Remove(int arr[], int size, int idx);
+
+bool Insert(int arr[], int size, int idx, int value);
+
+bool Append(int arr[], int size, int value);
+
+bool Invert(int arr[], int size);
+
+bool Join(int ori[], int ori_size, int join[], int join_size);
+
+enum SortType { ToMax = 0, ToMin = 1 };
+
+bool Sort(int arr[], int size, int type);
 
 #endif /* list.h */
