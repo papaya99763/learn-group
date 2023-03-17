a23494e358f08917ce8b3b907d19a12a4d754b74
waylon.yuen <waylon.yuen@gosmio.biz>
2023-03-17 09:29:48 +0800

    Add : 添加 string 定義宣告

--- src/string.h
+++ src/string.h
@@ -3,6 +3,18 @@
 #ifndef STRING_H
 #define STRING_H
 
+#define bool int
+#define true 1
+#define false 0
+
 void ToString(char str[]);
+
+int Size(char str[]);
+
+int Length(char str[]);
+
+bool Find(char str[], char word[], int *result);
+
+int Count(char str[], char word[]);
 
 #endif /* string.h */
