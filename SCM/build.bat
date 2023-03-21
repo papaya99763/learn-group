c1d23e87d81ab74ac7f191ef6a5046a9ba1e716b
waylon.yuen <waylon.yuen@gosmio.biz>
2023-03-21 14:20:48 +0800

    Modify : 修改 build.bat

--- SCM/build.bat
+++ SCM/build.bat
@@ -1,14 +1,18 @@
-ECHO Off
+@ECHO Off
 
 ECHO.
-ECHO Start to build
-ECHO ===================================
+ECHO ***********************************
+ECHO          [Start To Build]         
+ECHO ***********************************
+ECHO.
+
+ECHO 1. Check Arguments
+ECHO -----------------------------------
 SET dir_caller=%1
 SET target=%2
 SET tgt_name=%3
 ECHO [Main-Program]: "%target%"
 ECHO [Build-From]:   %dir_caller%
-
 
 REM Project
 SET dir_workspace=%~dp0..
@@ -33,17 +37,21 @@ if %target%==%demo% (
     SET tgt_build=%dir_main%\%main%
 )
 ECHO [Build-To]:     "%tgt_build%"
-ECHO.
-
-REM ====================================
 
 REM Target: Library
+ECHO.
+ECHO 2. Build library
+ECHO -----------------------------------
 SET dir_lib=%~dp0..\src
 SET library=
 SET library=%library% %dir_lib%\list.c
 ECHO [lib-build]: list.c
-REM SET library=%library% %dir_lib%\list.c
-REM ECHO [lib-build]: list.c
+SET library=%library% %dir_lib%\string.c
+ECHO [lib-build]: string.c
+SET library=%library% %dir_lib%\queue.c
+ECHO [lib-build]: queue.c
+SET library=%library% %dir_lib%\stack.c
+ECHO [lib-build]: stack.c
 
 REM Develop of Project
 if %target%==%demo% (
@@ -63,7 +71,28 @@ CD %dir_dev%\%build%
 REM Compile
 gcc %tgt_build% %library% -o %tgt_name%
 
+ECHO.
+ECHO ========= [Build Success] =========
+ECHO ***********************************
+ECHO.
+
+ECHO [%target%]: Start
+ECHO -----------------------------------
+ECHO.
 REM Execute the program
 %tgt_name%.exe
+ECHO.
+ECHO -----------------------------------
+
+SET errCode=%ERRORLEVEL%
+SET errStr=
+if %errCode% EQU 0 ( SET errStr=NoError
+) else ( SET errStr=Error)
+
+ECHO Program Return: %errCode% ( %errStr% )
+ECHO.
+ECHO * End of run ...
+ECHO.
 
 :end
+
