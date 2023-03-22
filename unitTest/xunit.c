661761962e3089306584450c4a8c9a35d74f04dc
waylon.yuen <waylon.yuen@gosmio.biz>
2023-03-22 11:51:40 +0800

    Add : 添加 單元測試函數定義

--- unitTest/xunit.c
+++ unitTest/xunit.c
@@ -6,11 +6,23 @@ bool XUnit_Assert_Int(int expected, int actual) {
 }
 
 bool XUnit_Assert_IntArray(int expected[], int actual[], int size) {
-    // todo:
-    return false;
+    for (int i = 0; i < size; i++) {
+        if(expected[i] != actual[i]) {
+            return false;
+        }
+    }
+    
+    return true;
 }
 
 bool XUnit_Assert_String(char expected[], char actual[]) {
-    // todo:
-    return false;
+    int idx = 0;
+    while(expected[idx] != '\0') {
+        if(expected[idx] != actual[idx]) {
+            return false;
+        }
+        idx++;
+    }
+    
+    return true;
 }
