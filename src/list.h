76854565bc563401f35b5a3a652a1361b01f6e5a
waylon.yuen <waylon.yuen@gosmio.biz>
2023-03-23 16:22:03 +0800

    Add : 添加 List 函式

--- src/list.h
+++ src/list.h
@@ -8,14 +8,39 @@
 enum Direction { Vertical = 0, Horizontal = 1 };
 void List_Show(int arr[], int size, enum Direction dir);
 
+int* List_Create(int size, int initValue);
+
+void List_Destroy(int *arr);
+
 void List_Init(int arr[], int size, int defaultValue);
+
+void List_Init_Increase(int arr[], int size, int startValue);
+
+void List_Init_Decrease(int arr[], int size, int startValue);
+
+void List_Init_Random(int arr[], int size, int min, int max);
+
+void List_Disrupted(int arr[], int size);
+
+void List_Invert(int arr[], int size);
+
+int* List_Clone(int arr[], int size);
 
 void List_Clear(int arr[], int size);
 
-// Obsolete:
-void Show_Sequence(int arr[], int size);
-// Obsolete:
-void Show_Invert(int arr[], int size);
+// ==== Query ==========================
+
+bool List_At(int arr[], int size, int idx, int *result_value);
+
+int List_Find(int arr[], int size, int value);
+
+int* List_Insert(int arr[], int *size, int idx, int value);
+
+int* List_Append(int arr[], int *size, int value);
+
+int* List_Remove(int arr[], int *size, int value);
+
+// =====================================
 
 bool At(int arr[], int size, int idx, int *result);
 
@@ -34,7 +59,5 @@ bool Join(int ori[], int ori_size, int join[], int join_size);
 enum SortType { ToMax = 0, ToMin = 1 };
 
 bool Sort(int arr[], int size, int type);
-
-void RandList(int arr[], int size, int min, int max);
 
 #endif /* list.h */
